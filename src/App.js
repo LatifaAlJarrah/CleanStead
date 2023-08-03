152f2705048a076f51337d54b2746e51dada8117
latifa al jarrah <latifa.200100@gmail.com>
2023-08-03 10:23:07 +0300

    add custom fonts to the website

--- src/App.js
+++ src/App.js
@@ -55,12 +55,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
+import "./fonts.css";
+
 const App = () => {
 
   const isRTL = true;
   return (
     <Router>
-      <div dir={isRTL ? 'rtl' : 'ltr'}>
+      <div dir={isRTL ? 'rtl' : 'ltr'}
+        style={{ fontFamily: 'CustomFont'}}>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
